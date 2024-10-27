1b57154a251e48fccdf15abfb45e4cf300db6737
codycannotcode <49048219+codycannotcode@users.noreply.github.com>
2024-10-27 15:27:41 -0700

    level grid generator works

--- Tanks/Assets/Scripts/LevelGenerator.cs
+++ Tanks/Assets/Scripts/LevelGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
@@ -20,38 +21,223 @@ public class LevelGenerator : MonoBehaviour
         GameObject level = new GameObject("Levels");
         int[,] tiles = new int[2,2];
 
-        List<string> availableSizes = new List<string>{"1x1"/*, "1x2", "2x1", "2x2"*/};
+        List<string> availableSizes = new List<string>{"1x1", "1x2", "2x1", "2x2"};
 
         //attempt to fill out tiles, iterate 4 times at most since all tiles will be filled with <= 4 layouts
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4 && availableSizes.Count > 0; i++) {
             string sizeChoice = availableSizes[Random.Range(0, availableSizes.Count)];
+            // Debug.Log("choice: " + sizeChoice);
             switch (sizeChoice) {
                 case "1x1":
-                List<int[]> availableTiles = new List<int[]>();
-                for (int r = 0; r < 2; r++) {
-                    for (int c = 0; c < 2; c++) {
-                        if (tiles[r, c] == 0) {
-                            availableTiles.Add(new int[]{r, c});
-                        }
-                    }
-                }
-                int[] tileChoice = availableTiles[Random.Range(0, availableTiles.Count)];
-                tiles[tileChoice[0], tileChoice[1]] = 1;
-                //mark a random available tile as taken
+                // {
+                //     List<int[]> availableTiles = new List<int[]>();
+                //     for (int r = 0; r < 2; r++) {
+                //         for (int c = 0; c < 2; c++) {
+                //             if (tiles[r, c] == 0) {
+                //                 availableTiles.Add(new int[]{r, c});
+                //             }
+                //         }
+                //     }
+                //     int[] tileChoice = availableTiles[Random.Range(0, availableTiles.Count)];
+                //     tiles[tileChoice[0], tileChoice[1]] = 1;
+                //     //mark a random available tile as taken
 
-                GameObject layoutChoice = layouts.size1x1[Random.Range(0, layouts.size1x1.Count)];
-                layoutChoice = Instantiate(layoutChoice);
-                layoutChoice.transform.SetParent(level.transform, true);
-                if (tileChoice[0] == 1 || tileChoice[1] == 1) {
-                    FlipLayout(layoutChoice.transform, tileChoice[1] == 1, tileChoice[0] == 1);
+                //     GameObject layoutChoice = layouts.size1x1[Random.Range(0, layouts.size1x1.Count)];
+                //     layoutChoice = Instantiate(layoutChoice);
+                //     layoutChoice.transform.SetParent(level.transform, true);
+                //     if (tileChoice[0] == 1 || tileChoice[1] == 1) {
+                //         FlipLayout(layoutChoice.transform, tileChoice[1] == 1, tileChoice[0] == 1);
+                //     }
+
+                //     //update available sizes list
+                //     //this is scuffed
+                //     switch (availableSizes.Count) {
+                //         case 4:
+                //         //if all 4 are available, only one of the tiles will be filled in, making 2x2 not possible
+                //         availableSizes.Remove("2x2");
+                //         break;
+                //         case 3:
+                //         //in diagonal cases, we can only place 1x1 layouts, remove all other sizes
+                //         //[0, 1]  [1, 0]
+                //         //[1, 0]  [0, 1]
+                //         if (tiles == new int[,]{{1,0},{0,1}} || tiles == new int[,]{{0,1},{1,0}}) {
+                //             availableSizes.Remove("2x1");
+                //             availableSizes.Remove("1x2");
+                //         }
+                //         //[1, 1]  [0, 0]
+                //         //[0, 0]  [1, 1]
+                //         else if (tiles == new int[,]{{1,1},{0,0}} || tiles == new int[,]{{0,0},{1,1}}) {
+                //             availableSizes.Remove("2x1");
+                //         }
+                //         //[1, 0]  [0, 1]
+                //         //[1, 0]  [0, 1]
+                //         else{
+                //             availableSizes.Remove("1x2");
+                //         }
+                //         break;
+                //         case 2:
+                //         //either removing 2x1 or 1x2
+                //         availableSizes.RemoveAt(1);
+                //         break;
+                //     }
+                // }
+                Choose1x1(tiles, availableSizes, level);
+                    break;
+                case "1x2":
+                {
+                    bool flipX = Random.Range(0, 2) == 0;
+                    bool flipZ;
+
+                    // if i is 0, then the matrix is empty, can put layout anywhere
+                    if (i == 0) {
+                        flipZ = Random.Range(0, 2) == 0;
+                        availableSizes.Remove("2x2");
+                        availableSizes.Remove("2x1");
+                    }
+                    //[0, 0]
+                    //[X, X]
+                    else if (tiles[0,0] == 0 && tiles[0, 1] == 0) {
+                        flipZ = false;
+                        availableSizes.Remove("1x2");
+                    }
+                    //[X, X]
+                    //[0, 0]
+                    else {
+                        flipZ = true;
+                        availableSizes.Remove("1x2");
+                    }
+
+                    //fill in the tiles that we're now using
+                    if (flipZ) {
+                        tiles[1, 0] = 1;
+                        tiles[1, 1] = 1;
+                    }
+                    else {
+                        tiles[0, 0] = 1;
+                        tiles[0, 1] = 1;
+                    }
+
+                    GameObject layoutChoice = layouts.size1x2[Random.Range(0, layouts.size1x2.Count)];
+                    layoutChoice = Instantiate(layoutChoice);
+                    layoutChoice.transform.SetParent(level.transform, true);
+
+                    FlipLayout(layoutChoice.transform, flipX, flipZ);
                 }
-                break;
+                    break;
+                case "2x1":
+                {
+                    bool flipX;
+                    bool flipZ = Random.Range(0, 2) == 0;
+
+                    // if i is 0, then the matrix is empty, can put layout anywhere
+                    if (i == 0) {
+                        flipX = Random.Range(0, 2) == 0;
+                        availableSizes.Remove("2x2");
+                        availableSizes.Remove("1x2");
+                    }
+                    //[0, X]
+                    //[0, X]
+                    else if (tiles[0, 0] == 0 && tiles[1, 0] == 0) {
+                        flipX = false;
+                        availableSizes.Remove("2x1");
+                    }
+                    else {
+                        flipX = true;
+                        availableSizes.Remove("2x1");
+                    }
+
+                    //fill in the tiles that we're now using
+                    if (flipX) {
+                        tiles[0, 1] = 1;
+                        tiles[1, 1] = 1;
+                    }
+                    else {
+                        tiles[0, 0] = 1;
+                        tiles[1, 0] = 1;
+                    }
+
+                    GameObject layoutChoice = layouts.size2x1[Random.Range(0, layouts.size2x1.Count)];
+                    layoutChoice = Instantiate(layoutChoice);
+                    layoutChoice.transform.SetParent(level.transform, true);
+
+                    FlipLayout(layoutChoice.transform, flipX, flipZ);
+                }
+                    break;
+                case "2x2":
+                {
+                    GameObject layoutChoice = layouts.size2x2[Random.Range(0, layouts.size2x2.Count)];
+                    layoutChoice = Instantiate(layoutChoice);
+                    layoutChoice.transform.SetParent(level.transform, true);
+
+                    FlipLayout(layoutChoice.transform, Random.Range(0, 2) == 0, Random.Range(0, 2) == 0);
+                    availableSizes.Clear();
+                }
+                    break;
             }
+            //[1, 1]
+            //[1, 1]
+            if (tiles[0,0] == 1 && tiles[0,1] == 1 && tiles[1,0] == 1 && tiles[1,1] == 1) {
+                availableSizes.Clear();
+            }
+            // string p = "\n";
+            // p += "[" + tiles[0,0] + ", " + tiles[0, 1] + "][" + tiles[1,0] + ", " + tiles[1, 1] + "]\n";
+            // Debug.Log(p);
         }
 
-
-
         navMeshSurface.BuildNavMesh();
+    }
+
+    void Choose1x1(int[,] tiles, List<string> availableSizes, GameObject level) {
+        List<int[]> availableTiles = new List<int[]>();
+        for (int r = 0; r < 2; r++) {
+            for (int c = 0; c < 2; c++) {
+                if (tiles[r, c] == 0) {
+                    availableTiles.Add(new int[]{r, c});
+                }
+            }
+        }
+        int[] tileChoice = availableTiles[Random.Range(0, availableTiles.Count)];
+        tiles[tileChoice[0], tileChoice[1]] = 1;
+        //mark a random available tile as taken
+
+        GameObject layoutChoice = layouts.size1x1[Random.Range(0, layouts.size1x1.Count)];
+        layoutChoice = Instantiate(layoutChoice);
+        layoutChoice.transform.SetParent(level.transform, true);
+        if (tileChoice[0] == 1 || tileChoice[1] == 1) {
+            FlipLayout(layoutChoice.transform, tileChoice[1] == 1, tileChoice[0] == 1);
+        }
+
+        //update available sizes list
+        //this is scuffed
+        switch (availableSizes.Count) {
+            case 4:
+            //if all 4 are available, only one of the tiles will be filled in, making 2x2 not possible
+            availableSizes.Remove("2x2");
+            break;
+            case 3:
+            //in diagonal cases, we can only place 1x1 layouts, remove all other sizes
+            //[0, 1]  [1, 0]
+            //[1, 0]  [0, 1]
+            if (tiles == new int[,]{{1,0},{0,1}} || tiles == new int[,]{{0,1},{1,0}}) {
+                availableSizes.Remove("2x1");
+                availableSizes.Remove("1x2");
+            }
+            //[1, 1]  [0, 0]
+            //[0, 0]  [1, 1]
+            else if (tiles == new int[,]{{1,1},{0,0}} || tiles == new int[,]{{0,0},{1,1}}) {
+                availableSizes.Remove("2x1");
+            }
+            //[1, 0]  [0, 1]
+            //[1, 0]  [0, 1]
+            else{
+                availableSizes.Remove("1x2");
+            }
+            break;
+            case 2:
+            //either removing 2x1 or 1x2
+            availableSizes.RemoveAt(1);
+            break;
+        }
     }
 
     static void FlipLayout(Transform transform, bool xAxis, bool zAxis) {
